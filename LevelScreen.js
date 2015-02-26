e9e251d8f6f227e105e69715dc07585f4391e5f9
r2d2Proton <Jerry.Richards@Outlook.com>
2015-02-26 13:35:08 -0700

    Adding LevelScreen

--- LevelScreen.js
+++ LevelScreen.js
@@ -1,3 +1,22 @@
+pc.script.attribute('levelShotsTexture', 'asset', [],
+{
+    type: 'texture',
+    max: 2
+});
+
+pc.script.attribute('selectBackTexture', 'asset', [],
+{
+    type: 'texture',
+    max: 1
+});
+
+pc.script.attribute('changeLevelTexture', 'asset', [],
+{
+    type: 'texture',
+    max: 1
+});
+
+
 pc.script.create('LevelScreen', function (context) {
     
     // Creates a new LevelScreen instance
@@ -7,8 +26,26 @@ pc.script.create('LevelScreen', function (context) {
         
         this.screenManager = null;
         this.gameManager = null;
+        this.soundManager = null;
+
+        this.realLevelShotsTextures = null;
+        this.realSelectBackTexture = null;
+        this.realChangeLevelTexture = null;
+
+        this.levels = ["RedSpace", "DoubleSpace"];
+
+        this.elapsedTime = 0.0;
     };
 
+    window.LevelScreen =
+    {
+        AssetLoadOrder: Object.freeze(
+        {
+            SelectBackTexture: 0,
+            ChangeLevelTexture: 1,
+            LevelShotsTextures: 2
+        })
+    };
 
     LevelScreen.prototype = {
         
@@ -17,10 +54,40 @@ pc.script.create('LevelScreen', function (context) {
             this.root = context.root.getChildren()[0];
             this.screenManager = this.root.script.ScreenManager;
             this.gameManager = this.root.script.GameManager;
+            this.soundManager = this.root.script.SoundManager;
+
+            this.elapsedTime = 0.0;
         },
         
         
-        SetFocus: function(focus) {
+        SetFocus: function (focus) {
+
+            if (focus) {
+
+                var assets = [
+                    context.assets.getAssetById(this.selectBackTexture),
+                    context.assets.getAssetById(this.changeLevelTexture)
+                ];
+
+                for (var x = 0; x < this.levelShotsTextures.length; x++) {
+                    assets[assets.length] = context.assets.getAssetById(this.levelShotsTextures[x]);
+                }
+
+                context.assets.load(assets).then(function (resources) {
+                    this.realSelectBackTexture = resources[window.LevelScreen.AssetLoadOrder.SelectBackTexture];
+                    this.realChangeLevelTexture = resource[window.LevelScreen.AssetLoadOrder.ChangeLevelTexture];
+
+                    this.realLevelShotsTextures = [];
+                    for (var x = 0; x < this.levelShotsTextures.length; x++) {
+                        this.realLevelShotsTextures[x] = resources[window.LevelScreen.AssetLoadOrder.LevelShotsTextures + x];
+                    }
+                }.bind(this));
+
+            } else {
+                this.realSelectBackTexture = null;
+                this.realChangeLevelTexture = null;
+                this.realLevelShotsTextures = null;
+            }
         },
         
         
@@ -28,11 +95,23 @@ pc.script.create('LevelScreen', function (context) {
         },
         
         
-        Update: function(dt) {
+        Update: function (dt) {
+            this.elapsedTime += dt;
         },
         
         
-        Draw3D: function(gd) {
+        Draw3D: function (gd) {
+
+            if (!gd)
+                return;
+
+            gd.clear({
+                color: [0.0, 0.0, 0.0, 1.0],
+                depth: 1.0,
+                flags: pc.CLEARFLAG_COLOR | pc.CLEARFLAG_DEPTH
+            });
+
+            this.screenManager.DrawBackground(gd);
         },
         
         
