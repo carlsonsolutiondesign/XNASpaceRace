faa18b2efddbe1121f5bcbdf90e669b101e8f623
r2d2Proton <Jerry.Richards@Outlook.com>
2015-02-27 12:29:17 -0700

    Adding PlayerShip.

---
+++ PlayerShip.js
@@ -0,0 +1,22 @@
+pc.script.create('PlayerShip', function (context) {
+
+    // Creates a new PlayerShip instance
+    var PlayShip = function (entity) {
+        this.entity = entity;
+    };
+
+
+    PlayerShip.prototype = {
+
+        // Called once after all resources are loaded and before the first update
+        initialize: function () {
+        },
+
+
+        // Called every frame, dt is time in seconds since last update
+        update: function (dt) {
+        }
+    };
+
+    return PlayerShip;
+});
