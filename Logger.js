f3e4f3c190300c1dad679552f449e145a2eec3c3
r2d2Proton <Jerry.Richards@Outlook.com>
2015-03-13 11:53:05 -0600

    Adding Node.js logging for packet playback.

---
+++ Logger.js
@@ -0,0 +1,65 @@
+﻿
+var fs = require('fs');
+
+function Log() {
+
+	this.debugLevel = null;
+
+	this.isOkay = false;
+	this.fileStream = null;
+
+	this.logFilename = null;
+}
+
+
+Log.debugLevel = Object.freeze({
+	All: {name: 'All', enabled: false },
+	Connection: { name: 'Connection', enabled: true },
+	Disconnect: { name: 'Disconnect', enabled: true },
+	UnrecognizedClient: { name: 'UnrecognizedClient', enabled: true },
+	ReportPlayers: { name: 'ReportPlayers', enabled: false },
+	ClientMessage: { name: 'ClientMessage', enabled: false },
+	ClientJoin: { name: 'ClientJoin', enabled: true },
+	ClientRejoin: { name: 'ClientRejoin', enabled: true },
+	ClientSpawn: { name: 'ClientSpawn', enabled: true },
+	ClientUpdate: { name: 'ClientUpdate', enabled: false },
+	ClientQuit: { name: 'ClientQuit', enabled: true },
+	DispatchMessages: { name: 'DispatchMessages', enabled: false },
+	NoDispatchMessages: { name: 'NoDispatchMessages', enabled: false },
+	DispatchNMessages: { name: 'DispatchNMessages', enabled: false },
+	WritePacket: { name: 'WritePacket', enabled: true },
+	AppendMessage: { name: 'AppendMessage', enabled: false }
+});
+
+
+Log.init = function (logFilename) {
+	try {
+		this.logFilename = logFilename;
+		this.fileStream = fs.createWriteStream(this.logFilename); //, { flags: 'a', encoding: null, fd: null, mode: 0666 });
+		this.isOkay = true;
+	} catch (err) {
+		console.log('Log.init error: ' + err.message);
+	}
+}
+
+
+Log.write = function (level, message) {
+	
+	if (typeof message === 'undefined')
+		message = '';
+
+	if (level.enabled || this.debugLevel.All.enabled) {
+		if (message && typeof message !== 'string')
+			message = JSON.stringify(message);
+		
+		var msg = level.name + ': ' + message;
+
+		if (this.isOkay) {
+			this.fileStream.write(msg + '\n');
+		}
+
+		console.log(msg);
+	}
+}
+
+module.exports = Log;
--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 
+var log = require('./Logger');
+log.init('rvbgames.log');
+
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -10,24 +13,6 @@ app.use(express.static(__dirname));
 http.listen(port, function () {
     console.log('listening on *:' + port);
 });
-
-var showDebug = Object.freeze({
-	All: false,
-	Connection: true,
-	Disconnect: true,
-	ReportPlayers: false,
-	ClientMessage: false,
-	ClientJoin: true,
-	ClientRejoin: true,
-	ClientSpawn: true,
-	ClientUpdate: false,
-	ClientQuit: true,
-	DispatchMessages: false,
-	NoDispatchMessages: false,
-	DispatchNMessages: false,
-	AppendMessage: false
-});
-
 
 function AppServer() {
 	
@@ -44,8 +29,7 @@ function AppServer() {
 	
 AppServer.prototype.ReportPlayers = function () {
 
-	if(showDebug.ReportPlayers || showDebug.All)
-		console.log('ReportPlayers');
+	log.write(log.debugLevel.ReportPlayers, 'ReportPlayers');
 
 	var numPlayers = 0;
 	
@@ -58,8 +42,7 @@ AppServer.prototype.ReportPlayers = function () {
 	
 AppServer.prototype.ClientMessage = function (socket, msg) {
 
-	if (showDebug.ClientMessage || showDebug.All)
-		console.log('ClientMessage ' + socket.client.id);
+	log.write(log.debugLevel.ClientMessage, socket.client.id);
 
 	this.AppendMessage('ServerMessage', "<" + this.thePlayers[socket.client.id].playerNumber + "> " + msg[0]);
 }
@@ -67,8 +50,7 @@ AppServer.prototype.ClientMessage = function (socket, msg) {
 	
 AppServer.prototype.ClientJoin = function (socket, packet) {
 
-	if (showDebug.ClientJoin || showDebug.All)
-		console.log('ClientJoin ' + socket.client.id);
+	log.write(log.debugLevel.ClientJoin, socket.client.id);
 	
 	this.thePlayers[socket.client.id] = {
 		playerNumber: this.maxplayers,
@@ -90,8 +72,7 @@ AppServer.prototype.ClientJoin = function (socket, packet) {
 	
 AppServer.prototype.ClientRejoin = function (socket, packet) {
 
-	if (showDebug.ClientRejoin || showDebug.All)
-		console.log('ClientRejoin ' + socket.client.id);
+	log.write(log.debugLevel.ClientRejoin, socket.client.id);
 	
 	var i = packet[0].indexOf("#");
 	
@@ -125,8 +106,7 @@ AppServer.prototype.ClientRejoin = function (socket, packet) {
 	
 AppServer.prototype.ClientSpawn = function (socket, packet) {
 
-	if (showDebug.ClientSpawn || showDebug.All)
-		console.log('ClientSpawn ' + socket.client.id);
+	log.write(log.debugLevel.ClientSpawn, socket.client.id);
 	
 	this.thePlayers[socket.client.id].shipId = packet.shipId;
 	this.thePlayers[socket.client.id].position = packet.position;
@@ -146,8 +126,7 @@ AppServer.prototype.ClientSpawn = function (socket, packet) {
 	
 AppServer.prototype.ClientUpdate = function (socket, packet) {
 
-	if (showDebug.ClientUpdate || showDebug.All)
-		console.log('ClientUpdate ' + socket.client.id);
+	log.write(log.debugLevel.ClientUpdate, socket.client.id);
 	
 	this.thePlayers[socket.client.id].shipId = packet.shipId;
 	this.thePlayers[socket.client.id].position = packet.position;
@@ -167,8 +146,7 @@ AppServer.prototype.ClientUpdate = function (socket, packet) {
 	
 AppServer.prototype.ClientQuit = function (socket, packet) {
 	
-	if (showDebug.ClientQuit || showDebug.All)
-		console.log('ClientQuit ' + socket.client.id);
+	log.write(log.debugLevel.ClientQuit, socket.client.id);
 
 	console.log(packet);
 }
@@ -176,19 +154,22 @@ AppServer.prototype.ClientQuit = function (socket, packet) {
 
 AppServer.prototype.DispatchMessages = function () {
 
-	if (showDebug.DispatchMessages || showDebug.All)
-		console.log('DispatchMessages');
+	log.write(log.debugLevel.DispatchMessages);
 
 	var len = this.messages.length;
 	if (len === 0) {
-		if (showDebug.NoDispatchMessages || showDebug.All)
-			console.log('No messages to dispatch');
+		log.write(log.debugLevel.NoDispatchMessages, 'No messages to dispatch');
 		return;
 	}
 
-	if (showDebug.DispatchNMessages || showDebug.All)
-		console.log('Dispatching ' + len + ' message(s)');
+	log.write(log.debugLevel.DispatchNMessages, 'Dispatching ' + len + ' message(s)');
 
+	if (log.debugLevel.WritePacket) {
+		for (var i = 0; i < len; i++) {
+			log.write(log.debugLevel.WritePacket, this.messages[i]);
+		}
+	}
+	
 	io.emit('ServerPackets', this.messages);
 
 	this.messages = [];
@@ -197,8 +178,7 @@ AppServer.prototype.DispatchMessages = function () {
 
 AppServer.prototype.AppendMessage = function (msg, packet) {
 
-	if (showDebug.AppendMessage || showDebug.All)
-		console.log('Append message: ' + msg);
+	log.write(log.debugLevel.AppendMessage, msg);
 
 	this.messages[this.messages.length] = { Msg: msg, Packet: packet };
 }
@@ -210,8 +190,7 @@ appServer.timer = setInterval(function () { appServer.DispatchMessages.call(appS
 
 io.on('connection', function (socket) {
 	
-	if (showDebug.Connection || showDebug.All)
-		console.log('connection made ' + socket.client.id);
+	log.write(log.debugLevel.Connection, socket.client.id);
 
 	socket.on('ClientMessage', function (msg) {
         if (appServer.thePlayers[socket.client.id]) {
@@ -242,7 +221,8 @@ io.on('connection', function (socket) {
         if (appServer.thePlayers[socket.client.id]) {
             appServer.ClientSpawn(socket, packet);
         } else {
-            console.log("Unrecognized client " + socket.client.id);
+			log.write(log.debugLevel.UnrecognizedClient, +socket.client.id);
+			socket.disconnect();
         }
     });
 			
@@ -251,7 +231,8 @@ io.on('connection', function (socket) {
         if (appServer.thePlayers[socket.client.id]) {
             appServer.ClientUpdate(socket, packet);
         } else {
-            console.log("Unrecognized client " + socket.client.id);
+			log.write(log.debugLevel.UnrecognizedClient, +socket.client.id);
+			socket.disconnect();
         }
     });
 
@@ -262,8 +243,7 @@ io.on('connection', function (socket) {
 
 	
 	socket.on('disconnect', function () {
-		if (showDebug.Disconnect|| showDebug.All)
-			console.log('disconnect ' + socket.client.id);
+		log.write(log.debugLevel.Disconnect, socket.client.id);
 
         if (appServer.thePlayers[socket.client.id]) {
             var response = {
