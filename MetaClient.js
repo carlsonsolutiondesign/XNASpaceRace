a8ff29fd09e0e22eb8bdf7db75333f23f086b3c1
carlsonsolutiondesign <john@carlsonsolutiondsign.com>
2015-03-16 16:24:40 -0500

    Simplified MetaClient.js

--- MetaClient.js
+++ MetaClient.js
@@ -1,60 +1,34 @@
-if (!pc) {
-	var pc = {};
-	pc.script = (function() {
-		var script = {
-			 create: function (name, callback) {
-				console.log(name);
-				callback()();
-			},
-			attribute: function() {
-			}
-		};
-		return script;
-	}());
-}
+$.get('http://xnaspacerace-44001.onmodulus.net:80/api/servers/', function(gameServers) {
+	    // build table headers
+	    var columnSet = [];
+	    var headerTr$ = $('<tr/>');
 
-pc.script.attribute('metaServer', 'string', 'http://xnaspacerace-44001.onmodulus.net',
-{
-    displayName: "Metaserver Connect String"
-});
+	    var key = "Connect String";
+	    columnSet.push(key);
+	    headerTr$.append($('<th/>').html(key));
 
-pc.script.create('MetaClient', function (context) {
-	var MetaClient = function (entity) {
-		this.entity = entity;
-		$.get('http://xnaspacerace-44001.onmodulus.net:80/api/servers/', function(gameServers) {
-			    // build table headers
-			    var columnSet = [];
-			    var headerTr$ = $('<tr/>');
+	    key = "# of Players";
+	    columnSet.push(key);
+	    headerTr$.append($('<th/>').html(key));
 
-			    var key = "Connect String";
-			    columnSet.push(key);
-			    headerTr$.append($('<th/>').html(key));
-
-			    key = "# of Players";
-			    columnSet.push(key);
-			    headerTr$.append($('<th/>').html(key));
-
-			    $("#metaServerTable").append(headerTr$);
+	    $("#metaServerTable").append(headerTr$);
 
 
-			    // build table body
-			    for (var host in gameServers) {
-				var row$ = $('<tr/>');
+	    // build table body
+	    for (var host in gameServers) {
+		var row$ = $('<tr/>');
 
-				var cellValue = host;
-				var td = $('<td/>');
-				var anchor = $('<a/>');
-				anchor.attr('href', cellValue);
-				anchor.html(cellValue);
-				td.append(anchor);
- 				row$.append(td);
+		var cellValue = host;
+		var td = $('<td/>');
+		var anchor = $('<a/>');
+		anchor.attr('href', cellValue);
+		anchor.html(cellValue);
+		td.append(anchor);
+		row$.append(td);
 
-				var cellValue = gameServers[host];
-				row$.append($('<td/>').html(cellValue));
+		var cellValue = gameServers[host];
+		row$.append($('<td/>').html(cellValue));
 
-				$("#metaServerTable").append(row$);
-			    }
-		});
-	};
-	return MetaClient;
+		$("#metaServerTable").append(row$);
+	    }
 });
